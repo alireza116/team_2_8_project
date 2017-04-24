2cd9a5971d06a216d0df51e612de5658e5124434
akarduni <akarduni@uncc.edu>
2017-04-23 22:09:34 -0400

    check out , check in, and login modals created and working

--- static/js/login.js
+++ static/js/login.js
@@ -6,3 +6,28 @@ $("#login-button").on("click",function(e){
 
     $("#login-modal").modal("toggle");
 });
+
+
+$("#login").on("click",function(e){
+    e.preventDefault();
+    var username  = $("#user-name").val();
+    var password = $("#password").val();
+    var logindata = {"username":username, "password":password};
+    $.ajax({
+        datatype: "json",
+        url: "http://127.0.0.1:5000/login",
+        data: logindata,
+        success: function(data){
+            console.log(data);
+            if (data =="logged in"){
+                $("#adminText").text("Admin Logged in");
+                $("#login-modal").modal("toggle");
+                d3.select("#login-button").style("pointer-events","none");
+            }
+            else {
+                alert(data)
+            }
+        }
+
+    })
+});
